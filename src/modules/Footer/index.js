19e35751329f1440959c8d77e94473dc9bcee692
tjuric03 <tonij.juric@hotmail.com>
2020-12-04 20:41:05 +0100

    Added simple Footer

--- src/modules/Footer/index.js
+++ src/modules/Footer/index.js
@@ -3,9 +3,40 @@ import styles from './style.module.css'
 
 const Footer = () => (
     <footer className={styles.footer}>
-        <h2>
-            This is the footer
-        </h2>
+        <div className={styles.categories}>
+            <div>
+                <h2>About the games</h2>
+                <ul>
+                    <li>Olympic Games</li>
+                    <li>Mascot</li>
+                    <li>Medals</li>
+                    <li>Anti-Doping</li>
+                    <li>Volunteers</li>
+                </ul>
+            </div>
+            <hr></hr>
+            <div>
+                <h2>FAQ</h2>
+                <ul>
+                    <li>Olympic Games</li>
+                    <li>Mascot</li>
+                    <li>Medals</li>
+                    <li>Anti-Doping</li>
+                    <li>Volunteers</li>
+                </ul>
+            </div>
+            <hr></hr>
+            <div>
+                <h2>Follow the games</h2>
+                <ul>
+                    <li>Olympic Games</li>
+                    <li>Mascot</li>
+                    <li>Medals</li>
+                    <li>Anti-Doping</li>
+                    <li>Volunteers</li>
+                </ul>
+            </div>
+        </div>
     </footer>
 )
 
