53edc85dc2bb3686998dcbfdbd8a31a01d26f5c5
Luka Cmrlec <lcmrle00@fesb.hr>
2021-01-19 08:05:42 +0100

    schedule final update

--- src/constants/events.js
+++ src/constants/events.js
@@ -3,39 +3,246 @@ const now = new Date()
 export default [
   {
     id: 0,
-    title: 'Football Semi-finals',
+    title: 'Football quarter-finals',
     allDay: false,
-    start: new Date("December 28, 2020 07:15:30"),
-    end: new Date("December 28, 2020 10:15:30"),
+    start: new Date("January 3, 2021 07:15:30"),
+    end: new Date("January 3, 2021 10:15:30"),
     sport: "Football",
-    location: "Something Arena"
+    location: "Honda field"
   },
   {
     id: 1,
     title: "Trampoline Woman's finals",
     allDay: false,
-    start: now,
-    end: now,
+    start: new Date("January 19, 2021 10:15:30"),
+    end: new Date("January 19, 2021 12:15:30"),
     sport: "Trampoline",
-    location: "Something Arena"
+    location: "Hiro Arena"
   },
   {
     id: 2,
     title: 'Basketball Croatia-USA',
     allDay: false,
-    start: now,
-    end: now,
+    start: new Date("January 19, 2021 10:15:30"),
+    end: new Date("January 19, 2021 12:15:30"),
     sport: "Basketball",
-    location: "Something Arena"
+    location: "Raijin Arena"
   },
   {
     id: 3,
     title: 'Swimming',
     allDay: false,
-    start: now,
-    end: now,
+    start: new Date("January 19, 2021 10:15:30"),
+    end: new Date("January 19, 2021 12:15:30"),
     sport: "Swimming",
-    location: "Something Arena"
+    location: "Genji Arena"
+  },
+  {
+    id: 4,
+    title: 'Football quarter-finals',
+    allDay: false,
+    start: new Date("January 7, 2021 14:15:30"),
+    end: new Date("January 7, 2021 16:15:30"),
+    sport: "Football",
+    location: "Honda field"
+  },
+  {
+    id: 5,
+    title: 'Football Semi-finals',
+    allDay: false,
+    start: new Date("January 12, 2021 16:15:30"),
+    end: new Date("January 12, 2021 18:15:30"),
+    sport: "Football",
+    location: "Honda field"
+  },
+  {
+    id: 6,
+    title: 'Football finals',
+    allDay: false,
+    start: new Date("January 22, 2021 18:15:30"),
+    end: new Date("January 22, 2021 20:15:30"),
+    sport: "Football",
+    location: "Honda field"
+  },
+  {
+    id: 7,
+    title: 'Swimming',
+    allDay: false,
+    start: new Date("January 16, 2021 10:15:30"),
+    end: new Date("January 16, 2021 12:15:30"),
+    sport: "Swimming",
+    location: "Genji Arena"
+  },
+  {
+    id: 8,
+    title: 'Swimming',
+    allDay: false,
+    start: new Date("January 18, 2021 7:15:30"),
+    end: new Date("January 18, 2021 11:15:30"),
+    sport: "Swimming",
+    location: "Genji Arena"
+  },
+  {
+    id: 9,
+    title: "Men's Long jump",
+    allDay: false,
+    start: new Date("January 5, 2021 10:15:30"),
+    end: new Date("January 5, 2021 12:15:30"),
+    sport: "Athletics",
+    location: "Tokyo Stadium"
+  },
+  {
+    id: 10,
+    title: "Javelin throw",
+    allDay: false,
+    start: new Date("January 9, 2021 16:15:30"),
+    end: new Date("January 9, 2021 18:15:30"),
+    sport: "Athletics",
+    location: "Tokyo Stadium"
+  },
+  {
+    id: 11,
+    title: "Women's 4000m",
+    allDay: false,
+    start: new Date("January 10, 2021 10:00:30"),
+    end: new Date("January 10, 2021 10:30:30"),
+    sport: "Athletics",
+    location: "Tokyo Stadium"
+  },
+  {
+    id: 12,
+    title: "Men's 100m qualifiers",
+    allDay: false,
+    start: new Date("February 1, 2021 20:00:30"),
+    end: new Date("February 1, 2021 20:15:30"),
+    sport: "Athletics",
+    location: "Tokyo Stadium"
+  },
+  {
+    id: 13,
+    title: "Men's 100m finals",
+    allDay: false,
+    start: new Date("February 3, 2021 20:00:30"),
+    end: new Date("February 3, 2021 20:15:30"),
+    sport: "Athletics",
+    location: "Tokyo Stadium"
+  },
+  {
+    id: 14,
+    title: "Shot put",
+    allDay: false,
+    start: new Date("January 8, 2021 10:00:30"),
+    end: new Date("January 8, 2021 12:00:30"),
+    sport: "Athletics",
+    location: "Tokyo Stadium"
+  },
+  {
+    id: 15,
+    title: 'Basketball semifinals',
+    allDay: false,
+    start: new Date("January 24, 2021 10:15:30"),
+    end: new Date("January 24, 2021 12:15:30"),
+    sport: "Basketball",
+    location: "Raijin Arena"
+  },
+  {
+    id: 16,
+    title: 'Basketball semifinals',
+    allDay: false,
+    start: new Date("January 27, 2021 10:00:30"),
+    end: new Date("January 27, 2021 12:30:30"),
+    sport: "Basketball",
+    location: "Raijin Arena"
+  },
+  {
+    id: 17,
+    title: 'Basketball finals',
+    allDay: false,
+    start: new Date("January 29, 2021 16:30:30"),
+    end: new Date("January 29, 2021 19:00:30"),
+    sport: "Basketball",
+    location: "Raijin Arena"
+  },
+  {
+    id: 18,
+    title: 'Cycling',
+    allDay: false,
+    start: new Date("January 29, 2021 10:30:30"),
+    end: new Date("January 29, 2021 12:00:30"),
+    sport: "Cycling",
+    location: "Honda Arena"
+  },
+  {
+    id: 19,
+    title: 'Cycling',
+    allDay: false,
+    start: new Date("January 22, 2021 10:30:30"),
+    end: new Date("January 22, 2021 12:00:30"),
+    sport: "Cycling",
+    location: "Honda Arena"
+  },
+  {
+    id: 20,
+    title: 'Cycling finals',
+    allDay: false,
+    start: new Date("February 2, 2021 10:30:30"),
+    end: new Date("February 2, 2021 12:00:30"),
+    sport: "Cycling",
+    location: "Honda Arena"
+  },
+  {
+    id: 22,
+    title: "Men's Fencing",
+    allDay: false,
+    start: new Date("January 23, 2021 10:30:30"),
+    end: new Date("January 23, 2021 12:00:30"),
+    sport: "Fencing",
+    location: "Honda Arena"
+  },
+  {
+    id: 23,
+    title: "Women's fencing",
+    allDay: false,
+    start: new Date("January 24, 2021 10:30:30"),
+    end: new Date("January 24, 2021 12:00:30"),
+    sport: "Fencing",
+    location: "Honda Arena"
+  },
+  {
+    id: 24,
+    title: "Women's tennis",
+    allDay: false,
+    start: new Date("January 13, 2021 10:30:30"),
+    end: new Date("January 13, 2021 12:00:30"),
+    sport: "Tennis",
+    location: "Edo Court"
+  },
+  {
+    id: 25,
+    title: "Men's tennis",
+    allDay: false,
+    start: new Date("January 14, 2021 10:30:30"),
+    end: new Date("January 14, 2021 12:00:30"),
+    sport: "Tennis",
+    location: "Edo Court"
+  },
+  {
+    id: 26,
+    title: "Women's tennis finals",
+    allDay: false,
+    start: new Date("January 20, 2021 10:30:30"),
+    end: new Date("January 20, 2021 12:00:30"),
+    sport: "Tennis",
+    location: "Edo Court"
+  },
+  {
+    id: 27,
+    title: "Men's tennis finals",
+    allDay: false,
+    start: new Date("January 22, 2021 10:30:30"),
+    end: new Date("January 22, 2021 12:00:30"),
+    sport: "Tennis",
+    location: "Edo Court"
   },
 ]
 
@@ -43,5 +250,9 @@ export const colors = {
   "Basketball": "#FF6600",
   "Football": "#33CC33",
   "Swimming": "#66CCFF",
-  "Trampoline": "#FF6666"
+  "Trampoline": "#FF6666",
+  "Athletics": "#CB4335",
+  "Cycling": "#990033",
+  "Fencing": "#404040",
+  "Tennis": "#F4D03F"
 };
